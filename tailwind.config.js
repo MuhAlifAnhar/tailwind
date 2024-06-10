264ecadde67bf4870a7887795a54c968a4693e64
Muh. Alif Anhar <sultankautsar21@gmail.com>
2024-06-10 10:27:15 +0700

    belajar tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'gentium': ['Gentium'],
+      },
+      colors: {
+        'alif': '#123',
+        'anhar': '#456'
+      },
+    },
+  },
+  plugins: [],
+}
+
